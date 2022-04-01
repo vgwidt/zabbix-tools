887e30117556668b63b3e2187bad46b398a93d15
vgwidt <vgwidt@live.com>
2022-04-01 09:38:37 +0900

    Added structure, implented menu loop and necessary cloning

--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,92 @@ use serde_json::json;
 use ::std::*;
 use std::io::Write;
 
+#[derive(Debug, Clone)]
+ struct Connection {
+        server: String,
+        username: String,
+        password: String
+ }
+
 fn main(){
 
     let mut zbxsrv = String::new();
     let mut user = String::new();
     let mut pass = String::new();
 
-    println!("Enter Zabbix Host IP/Name (Include virtual directory if it exists, e.g. 127.0.0.1/zabbix):");
-    io::stdin().read_line(&mut zbxsrv).expect("Failed to read line");
-    let zbxsrv: String = zbxsrv.trim().parse().expect("Invalid string!");
-    let zbxsrv = format!("http://{}/api_jsonrpc.php", zbxsrv);
+        println!("Enter Zabbix Host IP/Name (Include virtual directory if it exists, e.g. 127.0.0.1/zabbix):");
+        io::stdin().read_line(&mut zbxsrv).expect("Failed to read line");
+        let zbxsrv: String = zbxsrv.trim().parse().expect("Invalid string!");
+        let zbxsrv = format!("http://{}/api_jsonrpc.php", zbxsrv);
 
-    println!("Enter Username:");
-    io::stdin().read_line(&mut user).expect("Failed to read line");
-    let user: String = user.trim().parse().expect("Invalid string!");
+        println!("Enter Username:");
+        io::stdin().read_line(&mut user).expect("Failed to read line");
+        let user: String = user.trim().parse().expect("Invalid string!");
 
-    println!("Enter Password:");
-    io::stdin().read_line(&mut pass).expect("Failed to read line");
-    let pass: String = pass.trim().parse().expect("Invalid string!");
+        println!("Enter Password:");
+        io::stdin().read_line(&mut pass).expect("Failed to read line");
+        let pass: String = pass.trim().parse().expect("Invalid string!");
 
-    api_test(zbxsrv, user, pass).map_err(|err| println!("{:?}", err)).ok();
+        let conn_string = Connection {
+            server: zbxsrv,
+            username: user,
+            password: pass
+        };
 
-    println!("done");
+        let cloned_string = conn_string.clone();
+
+        api_test(conn_string).map_err(|err| println!("{:?}", err)).ok();
+
+loop {
+    let mut choice: String = String::new();
+        let conn_string = cloned_string.clone();
+        println!("Select option:");
+        println!("1: Add Hosts");
+        println!("2: Test API");
+        println!("3: Exit");
+        io::stdin().read_line(&mut choice).expect("Please enter a valid option");
+        let choice: i32 = choice.trim().parse().expect("Please type a number!");
+        
+        if choice == 1 {
+            add_hosts(conn_string).map_err(|err| println!("{:?}", err)).ok();
+            continue;
+        }
+        else if choice == 2 { 
+            api_test(conn_string).map_err(|err| println!("{:?}", err)).ok();
+            continue;
+        }
+        else if choice == 3 {
+            break;
+        }
+        else {
+            println!("Please select a valid option");
+            continue;
+        }
+    }
+    println!("Goodbye");
 }
 
 #[tokio::main]
-async fn api_test(s: String, u: String, p: String) -> Result<(), Box<dyn std::error::Error>> {
+async fn api_test(conn: Connection) -> Result<(), Box<dyn std::error::Error>> {
 
     //JSON Request Object
     let request = json!({
         "jsonrpc": "2.0",
         "method": "user.login",
         "params": {
-            "user": u,
-            "password": p
+            "user": conn.username,
+            "password": conn.password
         },
         "id": 1,
         "auth": null
     });
 
     //println!("Your request looks like:\n{}\n", serde_json::to_string_pretty(&request).unwrap());
-    println!("Your URL is {}", s);
+    println!("Your URL is {}", conn.server);
 
     let client = reqwest::Client::new();
 
-    let response = client.post(&s)
+    let response = client.post(&conn.server)
                          .json(&request)
                          .send()
                          .await?;
@@ -60,7 +101,7 @@ async fn api_test(s: String, u: String, p: String) -> Result<(), Box<dyn std::er
 }
 
 #[tokio::main]
-async fn add_hosts(s: String, u: String, p: String) -> Result<(), Box<dyn std::error::Error>> {
+async fn add_hosts(conn: Connection) -> Result<(), Box<dyn std::error::Error>> {
 /* host.create JSON structure
 {
     "jsonrpc": "2.0",
