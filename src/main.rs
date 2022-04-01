88cbffcd9f306e67629843a14658795987f3ea09
vgwidt <vgwidt@live.com>
2022-04-01 13:49:10 +0900

    add_hosts function setup, improved menu loop

--- src/main.rs
+++ src/main.rs
@@ -83,7 +83,7 @@ async fn api_test(conn: Connection) -> Result<(), Box<dyn std::error::Error>> {
     });
 
     //println!("Your request looks like:\n{}\n", serde_json::to_string_pretty(&request).unwrap());
-    println!("Your URL is {}", conn.server);
+    //println!("Your URL is {}", conn.server);
 
     let client = reqwest::Client::new();
 
@@ -102,36 +102,96 @@ async fn api_test(conn: Connection) -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::main]
 async fn add_hosts(conn: Connection) -> Result<(), Box<dyn std::error::Error>> {
-/* host.create JSON structure
-{
-    "jsonrpc": "2.0",
-    "method": "host.create",
-    "params": {
-        "host": "192.168.81.180",
-        "interfaces": [
-            {
-                "type": 1,
-                "main": 1,
-                "useip": 1,
-                "ip": "192.168.81.180",
-                "dns": "",
-                "port": "10050"
-            }
-        ],
-        "groups": [
-            {
-                "groupid": "15"
-            }
-        ],
-        "templates": [
-            {
-                "templateid": "10271"
-            }
-        ]
-    },
-    "auth": "'$token'",
-    "id": 1
-} */
+
+    let mut choice: String = String::new();
+    let mut ipaddress: String = String::new();
+    let mut hostname: String = String::new();
+    let mut groupid: String = String::new();
+    let mut authtoken: String = String::new();
+    println!("(Add Hosts) Select option:");
+    println!("1: Add Manually");
+    println!("2: Add from CSV");
+    println!("3: Exit");
+    io::stdin().read_line(&mut choice).expect("Please enter a valid option");
+    let choice: i32 = choice.trim().parse().expect("Please type a number!");
+
+    if choice == 1 {
+        println!("Enter IP Address:");
+        io::stdin().read_line(&mut ipaddress).expect("Failed to read line");
+        let ipaddress: String = ipaddress.trim().parse().expect("Invalid string!");
+
+        println!("Enter Host Name:");
+        io::stdin().read_line(&mut hostname).expect("Failed to read line");
+        let hostname: String = hostname.trim().parse().expect("Invalid string!");
+
+        println!("Enter Group ID:");
+        io::stdin().read_line(&mut groupid).expect("Failed to read line");
+        let groupid: String = groupid.trim().parse().expect("Invalid string!");
+
+        println!("Enter Auth Token ID:");
+        io::stdin().read_line(&mut authtoken).expect("Failed to read line");
+        let authtoken: String = authtoken.trim().parse().expect("Invalid string!");
+
+    }
+    else if choice == 2 { 
+        println!("Enter CSV file location (not yet working, will exit program");
+        return Ok(());
+    }
+    else if choice == 3 {
+        return Ok(());
+    }
+    else {
+        println!("Please select a valid option");
+    }
+    
+    let request: serde_json::Value = json!({
+        "jsonrpc": "2.0",
+        "method": "host.create",
+        "params": {
+            "host": hostname.trim(),
+            "name": hostname.trim(),
+            "interfaces": [
+                {
+                    "type": "SNMP",
+                    "ip": ipaddress.trim(),
+                    "port": "161",
+                    "details": {
+                        "community": "{$SNMP_COMMUNITY}",
+                    },
+                "interface_ref": "if1"
+                }
+            ],
+            "groups": [
+                {
+                    "groupid": groupid.trim()
+                }
+            ],
+            "templates": [
+                {
+                    "templateid": "10607"
+                }
+            ],
+            "inventory_mode": "DISABLED"
+        },
+        "auth": authtoken.trim(),
+        "id": 1
+    });
+
+    println!("Your request looks like:\n{}\n", serde_json::to_string_pretty(&request).unwrap());
+
+    let client = reqwest::Client::new();
+
+    let response = client.post(&conn.server)
+                         .json(&request)
+                         .send()
+                         .await?;
+
+    let content: serde_json::Value = response.json().await?;
+    
+    //If you receive JSON result back, connection appears to be successful.  Error should be passed otherwise on connection failure.
+    println!("{:#?}", content);
+
+
 
 Ok(())
 
